2617c9ceebfedd886f9dae31fe5646474bdf3cff
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2026-02-22 12:25:03 +0100

    first arg labelless test

--- Sources/Path/standard-path/standard-path-overloads.swift
+++ Sources/Path/standard-path/standard-path-overloads.swift
@@ -1,12 +1,25 @@
-// as convenient labelless API:
-// .init(.home, "my_path")
+// // as convenient labelless API:
+// // .init(.home, "my_path")
+// extension StandardPath {
+//     public init(
+//         _ components: any StandardPathComponent...,
+//         filetype: AnyFileType? = nil
+//     ) {
+//         self.segments = components.flatMap { $0.toPathSegments() }
+//         self.filetype = filetype
+//     }
+// }
 extension StandardPath {
     public init(
-        _ components: any StandardPathComponent...,
+        _ basepath: StandardPath,
+        _ segments: String...,
         filetype: AnyFileType? = nil
     ) {
-        self.segments = components.flatMap { $0.toPathSegments() }
-        self.filetype = filetype
+        self.init(
+            from: basepath,
+            segments,
+            filetype: filetype
+        )
     }
 }
 
@@ -62,3 +75,4 @@ extension StandardPath {
         self.init(from: basepath, segments, filetype: filetype)
     }
 }
+
